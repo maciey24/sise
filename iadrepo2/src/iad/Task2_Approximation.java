2a8b072ad5b77a1f239ba23f2ce0e6f7d6f1e074
Maciek <189789@edu.p.lodz.pl>
2017-06-03 15:16:45 +0200

    zmiany parametrów, komentarze

--- iadrepo2/src/iad/Task2_Approximation.java
+++ iadrepo2/src/iad/Task2_Approximation.java
@@ -25,17 +25,17 @@ public class Task2_Approximation {
 	public static void main(String[] args) 
         {
             double krok = 0.01;
-            for(double i = 1; i<=2; i+=krok) //
+            for(double i = 1; i<=100; i+=krok) //
             {
                 aproksymowanaFunkcja.add(new Point(0.0, i, Math.sqrt(i)));
                 testX.add(i);
             }
             Main.PlotPoints(aproksymowanaFunkcja);
             
-            int liczbaPunktowTreningowych = 50;
+            int liczbaPunktowTreningowych = 50;//liczba punktow treningowych
             for(int i = 0; i<liczbaPunktowTreningowych; i++)
             {
-                double x = ThreadLocalRandom.current().nextDouble(1, 100);
+                double x = ThreadLocalRandom.current().nextDouble(1, 100); //zakres dziedzina punktow treningowych
                 Double[] tab = new Double[1];
                 tab[0]=x;
                 trainX.add(tab);
@@ -64,19 +64,19 @@ public class Task2_Approximation {
 //                
 //                Main.PlotPoints(wszystkiePunkty, "Wykres danych treningowych i wzorcowych dla sieci", "x", "y", "Dane treningowe", "Dane wzorcowe");
 		// 1 - 20
-                wszystkiePunkty.addAll(aproksymowanaFunkcja);
+//                wszystkiePunkty.addAll(aproksymowanaFunkcja);
                 wszystkiePunkty.addAll(trainPoints);
-		for (int i=10; i<=10; i++) 
+		for (int i=10; i<=10; i++) //liczba neuronow w kazdej warstwie ukrytej
                 {
-                    NeuralNetwork net = new NeuralNetwork(1, 4, i, 1, Boolean.FALSE);
-                    net.train(trainX, trainY, 0.0005, 0.6, 0.0001, 15000);
+                    NeuralNetwork net = new NeuralNetwork(1, 4, i, 1, Boolean.FALSE);//druga liczba - liczba warstw, warstwy
+                    net.train(trainX, trainY, 0.0005, 0.6, 0.01, 15000);//predkosc uczenia, ped momentum, max blad, liczba epok epoki
 
                     Double netError = 0.0;
-                    for(int j=0; j<trainPoints.size(); j++) 
+                    for(int j=0; j<aproksymowanaFunkcja.size(); j++) 
                     {
-                        Double res =  net.fwdPropagate(new Double [] { trainPoints.get(j).getVals()[0] })[0];
-                        c(trainPoints.get(j).getVals()[0]+ " :" + res);
-                        results.add(new Point(2.0, trainPoints.get(j).getVals()[0], res));
+                        Double res =  net.fwdPropagate(new Double [] { aproksymowanaFunkcja.get(j).getVals()[0] })[0];
+                        c(aproksymowanaFunkcja.get(j).getVals()[0]+ " :" + res);
+                        results.add(new Point(2.0, aproksymowanaFunkcja.get(j).getVals()[0], res));
                         netError += res*res;
                     }
                     Main.PlotPoints(results, "wykres odpowiedzi sieci na zbiór testowy dla "+i+" neuronow w warstwie ukrytej",
